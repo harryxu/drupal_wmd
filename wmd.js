fa2365ef8d08b49d8031cb6869b0e6cf6091ce89
harryxu <harryzhxu@gmail.com>
2011-01-17 00:10:04 +0800

    starting port to drupal 7

--- wmd.js
+++ wmd.js
@@ -1,10 +1,11 @@
 ;(function($){
 
 $(function(){
-    var editor = $('#edit-body');
+    var editor = $('#edit-body-und-0-value');
     editor.before($('<div id="wmd-toolbar" class="wmd-toolbar"></div>'));
-    editor.parent().append($('<div id="wmd-preview" class="wmd-preview"></preview>'));
-    new WMD("edit-body", "wmd-toolbar", { preview: "wmd-preview" });
+    //editor.parent().append($('<div id="wmd-preview" class="wmd-preview"></preview>'));
+    new WMD("edit-body-und-0-value", "wmd-toolbar");
+    //new WMD("edit-body-und-0-value", "wmd-toolbar", { preview: "wmd-preview" });
 });
 
 })(jQuery)
