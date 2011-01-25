0b517ecde173ce369976963b60eff801d1c9b7a4
harryxu <harryzhxu@gmail.com>
2011-01-26 00:12:41 +0800

    fixed textarea finding

--- wmd.js
+++ wmd.js
@@ -4,11 +4,11 @@ $(function(){
     var editor = $('#edit-body-und-0-value');
     editor.before($('<div id="wmd-toolbar" class="wmd-toolbar"></div>'));
     //editor.parent().append($('<div id="wmd-preview" class="wmd-preview"></preview>'));
-    var ta = $('#edit-body').find('textarea');
+    var ta = $('#edit-body').find('textarea.text-full');
     if (ta.length > 0) {
         var taid = ta.attr('id');
         new WMD(taid, "wmd-toolbar");
     }
 });
 
-})(jQuery)
+})(jQuery);
