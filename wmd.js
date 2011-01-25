3ea8efe2d09a2cca5a249002e1d4202015003003
harryxu <harryzhxu@gmail.com>
2011-01-26 00:04:17 +0800

    fixed body textarea finding

--- wmd.js
+++ wmd.js
@@ -4,8 +4,11 @@ $(function(){
     var editor = $('#edit-body-und-0-value');
     editor.before($('<div id="wmd-toolbar" class="wmd-toolbar"></div>'));
     //editor.parent().append($('<div id="wmd-preview" class="wmd-preview"></preview>'));
-    new WMD("edit-body-und-0-value", "wmd-toolbar");
-    //new WMD("edit-body-und-0-value", "wmd-toolbar", { preview: "wmd-preview" });
+    var ta = $('#edit-body').find('textarea');
+    if (ta.length > 0) {
+        var taid = ta.attr('id');
+        new WMD(taid, "wmd-toolbar");
+    }
 });
 
 })(jQuery)
