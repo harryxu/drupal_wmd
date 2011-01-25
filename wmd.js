f082e4c6cfbe5c9a7d8ab6b7380d4ebd78d700f2
harryxu <harryzhxu@gmail.com>
2011-01-26 01:05:56 +0800

    this is really textarea finding fix

--- wmd.js
+++ wmd.js
@@ -1,12 +1,10 @@
 ;(function($){
 
 $(function(){
-    var editor = $('#edit-body-und-0-value');
-    editor.before($('<div id="wmd-toolbar" class="wmd-toolbar"></div>'));
-    //editor.parent().append($('<div id="wmd-preview" class="wmd-preview"></preview>'));
-    var ta = $('#edit-body').find('textarea.text-full');
-    if (ta.length > 0) {
-        var taid = ta.attr('id');
+    var editor = $('#edit-body').find('textarea.text-full');
+    if (editor.length > 0) {
+        editor.before($('<div id="wmd-toolbar" class="wmd-toolbar"></div>'));
+        var taid = editor.attr('id');
         new WMD(taid, "wmd-toolbar");
     }
 });
